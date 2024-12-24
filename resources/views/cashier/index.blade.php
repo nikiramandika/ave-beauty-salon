62cae8e5e374ca59cdf2f39fb9325ab40d5aa432
nikiramandika <nikiramandika@icloud.com>
2024-12-24 07:52:15 +0700

    final

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -97,14 +97,14 @@
                         </button>
                     </div>
 
-                    <!-- Search Bar -->
+                    {{-- <!-- Search Bar -->
                     <div class="mb-4">
                         <div class="relative">
                             <input type="text" placeholder="..."
                                 class="w-full pl-10 pr-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-pink-500">
                             <i class="fas fa-search absolute left-3 top-3 text-gray-400"></i>
                         </div>
-                    </div>
+                    </div> --}}
                     <!-- Product Grid -->
                     <div id="productsSection" class="section block" >
                         <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
